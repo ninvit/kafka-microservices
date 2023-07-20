4d4339c5085b8fc33a07eb713b70dadeb1eceb92
ninvit <ninvit@gmail.com>
2023-07-19 23:39:43 -0300

    first commit

---
+++ base-domains/src/test/java/com/ninvit/basedomains/BaseDomainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninvit.basedomains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDomainsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
